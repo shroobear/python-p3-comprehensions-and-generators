3ba4f49ac5de27380d2db7d2da2eef25ff602896
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-07-10 15:00:44 -0400

    lab complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list = [n for n in num_list if n % 2 == 0]
+    return(even_list)
 
 def make_exclamation(sentence_list):
-    pass
+    excited_list = [string.replace(string, (string + "!")) for string in sentence_list]
+    return excited_list
